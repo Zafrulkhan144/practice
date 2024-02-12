c45d24381638e3d4a2b5970d7b14655f2b562e6c
LENOVO <LENOVO@DESKTOP-S2NLEGU>
2024-02-12 09:15:41 +0530

    commit

--- src/swing/git.java
+++ src/swing/git.java
@@ -4,4 +4,8 @@ public class git {
     public static void main(String[] args) {
     	System.out.println("Hello Worl");
     }
+    
+    public void display() {
+    	System.out.println("Hello World");
+    }
 }
